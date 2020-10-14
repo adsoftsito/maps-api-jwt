b56ba928f9555fda81b9ce1dcb2dca052cf565ff
adsoftsito <adsoft@live.com.mx>
2020-10-14 02:12:35 -0500

    update municipio controller

--- src/main/java/com/grokonez/jwtauthentication/repository/MunicipioRepository.java
+++ src/main/java/com/grokonez/jwtauthentication/repository/MunicipioRepository.java
@@ -9,12 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
-//    List<Capa> findCapasWithOutGeometryByActivo(Boolean activo);
     
-
-//    @Query(value ="select idestado, idmunicipio, municipio  from Municipios where idestado = ?1")
     @Query(value = "select NEW com.grokonez.jwtauthentication.model.Municipio(c.idestado, c.idmunicipio, c.municipio) from Municipio c where c.idestado=?1 order by c.idmunicipio")
     List<Municipio> findMunicipiosByEstado(Long idestado);
 
-    //Integer findCapaByName(String name);
 }
